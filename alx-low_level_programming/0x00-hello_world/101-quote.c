ebb747b7bc364f421b2d81bcf627489f91b2fd51
kithkip <kithkinyongi@gmail.com>
2024-05-13 07:22:26 -0400

    hello world

---
+++ alx-low_level_programming/0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a quote
+ *
+ * Return: 1 - success
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
